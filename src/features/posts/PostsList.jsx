85850b58c594adfb7f71cbfe84d73f7286529190
akambetov <almat_kambetov@mail.ru>
2022-08-28 23:40:02 +0600

    PostsList: render loading, failed && succedded statuses of fetching posts action

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -2,33 +2,49 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 import { fetchPosts, selectAllPosts } from './postsSlice'
-import { IDLE } from '../../constants'
+import { FAILED, IDLE, LOADING, SUCCEEDED } from '../../constants'
+
+const PostExcerpt = ({ post }) => (
+  <article className="post-excerpt">
+    <h3>{post.title}</h3>
+    <div>
+      <PostAuthor userId={post.user} />
+      <TimeAgo timestamp={post.date} />
+    </div>
+    <p className="post-content">{post.content.substring(0, 100)}</p>
+    <ReactionButtons post={post} />
+    <Link to={`/posts/${post.id}`} className="button muted-button">
+      View Post
+    </Link>
+  </article>
+)
 
 export const PostsList = () => {
   const dispatch = useDispatch()
   const postsStataus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
   const posts = useSelector(selectAllPosts)
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderPosts = (posts) =>
-    posts.map((post) => (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-        <ReactionButtons post={post} />
-        <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
-      </article>
+  let content
+
+  if (postsStataus === LOADING) {
+    content = <Spinner text="loading..." />
+  } else if (postsStataus === SUCCEEDED) {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
     ))
+  } else if (postsStataus === FAILED) {
+    content = <div>{error}</div>
+  }
 
   useEffect(() => {
     postsStataus === IDLE && dispatch(fetchPosts())
@@ -37,7 +53,7 @@ export const PostsList = () => {
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderPosts(orderedPosts)}
+      {content}
     </section>
   )
 }
