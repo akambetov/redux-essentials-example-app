6c096d11e494c936c52636817b590421e78a431d
akambetov <almat_kambetov@mail.ru>
2022-08-28 17:28:02 +0600

    postAdded: prepera (prepera action.payload)

--- src/features/posts/AddPostFrom.jsx
+++ src/features/posts/AddPostFrom.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
 
 import { postAdded } from './postsSlice'
 
@@ -17,7 +16,7 @@ export const AddNewPost = () => {
     event.preventDefault()
 
     if (title && content) {
-      dispatch(postAdded({ id: nanoid(), title, content }))
+      dispatch(postAdded(title, content))
       setTitle('')
       setContent('')
     }
