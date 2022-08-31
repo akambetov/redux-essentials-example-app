c260ae30f55cc9ddd72c301a8ea227a63a54b837
akambetov <almat_kambetov@mail.ru>
2022-09-01 00:23:11 +0600

    feat: render SinglePostPage via RTK Q

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
-import { selectPostById } from './postsSlice'
+import { useGetPostQuery } from '../api/apiSlice'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
+  const {
+    data: post,
+    isFetching,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPostQuery(postId)
 
-  const post = useSelector((state) => selectPostById(state, postId))
-
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
-    )
-  }
-
-  return (
-    <section>
+  let content
+  if (isFetching) {
+    content = <Spinner text="loading" />
+  } else if (isSuccess) {
+    content = (
       <article className="post">
         <h2>{post.title}</h2>
         <PostAuthor userId={post.user} />
@@ -32,6 +32,15 @@ export const SinglePostPage = ({ match }) => {
           Edit Post
         </Link>
       </article>
-    </section>
-  )
+    )
+  } else if (isError) {
+    content = (
+      <>
+        <h2>Post not found!</h2>
+        <p>{error.error}</p>
+      </>
+    )
+  }
+
+  return <section>{content}</section>
 }
